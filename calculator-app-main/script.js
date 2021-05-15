6aea1e1f9c9b5b445450c9582ae777628e43a266
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2021-05-15 11:43:08 +0530

    Style guide details alone added

---
+++ calculator-app-main/script.js
@@ -0,0 +1,51 @@
+const themes = [
+  {
+    '--main-bg': 'hsl(222, 26%, 31%)',
+    '--keypad-bg': 'hsl(223, 31%, 20%)',
+    '--screen-bg': 'hsl(224, 36%, 15%)',
+
+    '--action-key-bg': 'hsl(225, 21%, 49%)',
+    '--action-key-shadow': 'hsl(224, 28%, 35%)',
+    '--toggle-bg': 'hsl(6, 63%, 50%)',
+    '--toggle-shadow': 'hsl(6, 70%, 34%)',
+
+    '--key-bg': 'hsl(30, 25%, 89%)',
+    '--key-shadow': 'hsl(28, 16%, 65%)',
+
+    '--text': 'hsl(221, 14%, 31%)',
+    '--secondary': 'hsl(0, 0, 100%)',
+  },
+  {
+    '--main-bg': 'hsl(0, 0%, 90%)',
+    '--keypad-bg': 'hsl(0, 5%, 81%)',
+    '--screen-bg': 'hsl(0, 0%, 93%)',
+
+    '--action-key-bg': 'hsl(185, 42%, 37%)',
+    '--action-key-shadow': 'hsl(185, 58%, 25%)',
+    '--toggle-bg': 'hsl(25, 98%, 40%)',
+    '--toggle-shadow': 'hsl(25, 99%, 27%)',
+
+    '--key-bg': 'hsl(30, 25%, 89%)',
+    '--key-shadow': 'hsl(28, 16%, 65%)',
+
+    '--text': 'hsl(60, 10%, 19%)',
+    '--secondary': 'hsl(0, 0, 100%)',
+  },
+  {
+    '--main-bg': 'hsl(268, 75%, 9%)',
+    '--keypad-bg': 'hsl(268, 71%, 12%)',
+    '--screen-bg': 'hsl(268, 71%, 12%)',
+
+    '--action-key-bg': 'hsl(281, 89%, 26%)',
+    '--action-key-shadow': 'hsl(285, 91%, 52%)',
+    '--toggle-bg': 'hsl(176, 100%, 44%)',
+    '--toggle-shadow': 'hsl(177, 92%, 70%)',
+
+    '--key-bg': 'hsl(268, 47%, 21%)',
+    '--key-shadow': 'hsl(290, 70%, 36%)',
+
+    '--text': 'hsl(52, 100%, 62%)',
+    '--secondary': 'hsl(0, 0, 100%)',
+    '--tertiary': 'hsl(198, 20%, 13%)',
+  },
+];
